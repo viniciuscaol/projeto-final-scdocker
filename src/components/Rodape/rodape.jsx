cfeefe4b42e2a909f24d9ce7bddd41e6891aaf4b
Roosevelt Franklin <roosevelt_franklin@hotmail.com>
2024-02-02 16:47:46 -0300

    rodapé finalizado

--- src/components/Rodape/rodape.jsx
+++ src/components/Rodape/rodape.jsx
@@ -1,8 +1,24 @@
+import styled from "styled-components";
+
+const RodapeContainer = styled.div`
+    width: 100%;
+    height: 120px;
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    background-color: #1F0049;
+    color: #FFFFFF;
+`
+
+const RodapeText = styled.p`
+    padding: 0 15px;
+`
+
 function Rodape() {
     return (
-        <div className="rodape">
-            <h1>Isto é o meu rodape</h1>
-        </div>
+        <RodapeContainer>
+            <RodapeText>Copyright © 2024 - Site desenvolvido por turma 1025.</RodapeText>
+        </RodapeContainer>
     )
 }
 export default Rodape
