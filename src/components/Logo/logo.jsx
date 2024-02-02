8d3f3cf7db0fe5cecea6f193411478b34580ca4e
Roosevelt Franklin <roosevelt_franklin@hotmail.com>
2024-02-02 16:03:25 -0300

    componente menu em andamento

--- src/components/Logo/logo.jsx
+++ src/components/Logo/logo.jsx
@@ -1,6 +1,17 @@
+import styled from "styled-components";
+
+const LogoContainer = styled.div`
+    width: 250px;
+`
+const LogoImage = styled.img`
+    height: 120px;
+`
 function Logo() {
     return (
-            <h1>Logo</h1>
+            <LogoContainer>
+                <LogoImage src="./images/logo.svg" alt="Logomarca da casa de eventos" />
+            </LogoContainer>
     )
 }
+
 export default Logo
--- src/components/Menu/menu.jsx
+++ src/components/Menu/menu.jsx
@@ -1,6 +1,35 @@
+import styled from "styled-components";
+import { Link } from 'react-router-dom'
+
+const MenuContainer = styled.div`
+
+`
+
+const MenuList = styled.ul`
+
+`
+
+const MenuItem = styled.li`
+
+`
+
+const MenuLink = styled(Link)`
+
+`
+
+
 function Menu() {
     return (
-            <h1>Menu</h1>
+            <MenuContainer>
+                <MenuList>
+                    <MenuItem>
+                        <MenuLink to="/homepage">Homepage</MenuLink>
+                    </MenuItem>
+                    <MenuItem>
+                        <MenuLink to="/contato">Contato</MenuLink>
+                    </MenuItem>
+                </MenuList>
+            </MenuContainer>
     )
 }
 export default Menu
