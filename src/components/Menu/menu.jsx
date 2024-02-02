f870c02d90affc3e3600dbebc752145602d675eb
Roosevelt Franklin <roosevelt_franklin@hotmail.com>
2024-02-02 16:42:25 -0300

    menu finalizado

--- src/components/Menu/menu.jsx
+++ src/components/Menu/menu.jsx
@@ -2,19 +2,28 @@ import styled from "styled-components";
 import { Link } from 'react-router-dom'
 
 const MenuContainer = styled.div`
-
+    width: calc(100% - 250px);
+    height: 120px;
+    display: flex;
+    justify-content: right;
+    align-items: center;
 `
 
 const MenuList = styled.ul`
-
+    display: flex;
 `
 
 const MenuItem = styled.li`
-
+    list-style-type: none;
 `
 
 const MenuLink = styled(Link)`
-
+    color: #E2D8A5;
+    text-decoration: none;
+    padding: 0 15px;
+    &:hover {
+        color: #6F9F9C;
+    }
 `
 
 
