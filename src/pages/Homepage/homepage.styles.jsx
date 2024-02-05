91764956d7f30f5e66395e1c5daf41d2bd2ed6d6
Roosevelt Franklin <roosevelt_franklin@hotmail.com>
2024-02-05 17:08:59 -0300

    lista de eventos com css

--- src/pages/Homepage/homepage.styles.jsx
+++ src/pages/Homepage/homepage.styles.jsx
@@ -30,14 +30,29 @@ export const EventosLista = styled.ul`
     padding: 20px 0 40px 0;
 `
 export const EventosItem = styled.li`
-   
+    width: 360px;
+    min-height: 320px;
+    background-color: #FFFFFF;
+    -webkit-box-shadow: 0px 0px 10px 1px rgba(0,0,0,0.3);
+    -moz-box-shadow: 0px 0px 10px 1px rgba(0,0,0,0.3);
+    box-shadow: 0px 0px 10px 1px rgba(0,0,0,0.3);
+    list-style-type: none;
 `
-export const EventoTitulo = styled.h2`
-   
+export const EventoTitulo = styled.h3`
+    font-family: 'Playfair Display', serif;
+    font-size: 18px;
+    padding: 15px 15px 0 15px;
+    font-weight: 300;
+    line-height: 150%;
 `
 export const EventoImagem = styled.img`
-   
+    padding: 15px;
+    width: 100%;
+    aspect-ratio: 16/10; 
+    object-fit: cover;
 `
 export const EventoDataHora = styled.p`
-   
+   font-size: 14px;
+   padding: 15px;
+   line-height: 150%;
 `
