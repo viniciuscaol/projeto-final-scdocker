821037da6f6e7c16e58505ad79c0fc90e27fd342
Roosevelt Franklin <roosevelt_franklin@hotmail.com>
2024-02-05 17:46:44 -0300

    Componente de login funcionando

--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -1,11 +1,39 @@
-import { Link } from 'react-router-dom'
-function Login() {
-    return (
-        <div className="login">
-            <h1>Formulário de login aqui</h1>
-            <Link to="/homepage">Ir para Homepage</Link>
-        </div>
-    )
-}
+import { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 
-export default Login
+
+const Login = () => {
+    const [nome, setNome] = useState('');
+    const [senha, setSenha] = useState('');
+    const [erro, setErro] = useState('');
+    const navigate = useNavigate();
+
+    const EfetuarLogin = async () => {
+        try {
+            const response = await axios.get('http://localhost:3000/usuarios');
+            const usuarios = response.data;
+            const usuarioEncontrado = usuarios.find(usuario => usuario.nome === nome && usuario.senha === senha);
+            if (usuarioEncontrado) {
+                navigate('/homepage');
+            } else {
+                setErro('Nome ou senha inválido');
+            }
+        } catch (error) {
+            console.error('Erro ao buscar eventos', error);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <input type="text" placeholder="Nome" value={nome} onChange={e => setNome(e.target.value)} />
+            <input type="password" placeholder="Senha" value={senha} onChange={e => setSenha(e.target.value)} />
+            <button onClick={EfetuarLogin}>Entrar</button>
+            {erro && <p>{erro}</p>}
+        </div>
+    );
+
+};
+
+export default Login;
