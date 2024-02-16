2b597d9fc8a3f228c7fdca8d0edb917703417356
Roosevelt Franklin <roosevelt_franklin@hotmail.com>
2024-02-16 15:02:00 -0400

    Tela de login finalizada

--- src/pages/Login/login.styles.jsx
+++ src/pages/Login/login.styles.jsx
@@ -8,7 +8,13 @@ export const LoginContainer = styled.div`
     font-weight: 400;
 `
 
-export const LoginTitle = styled.h1``
+export const LoginTitle = styled.h1`
+    font-family: 'Playfair Display', serif;
+    text-align: center;
+    font-size: 28px;
+    font-weight: 400;
+    color: #1F0049;
+`
 
 export const Sidebar = styled.aside`
     color: #FFFFFF;
@@ -16,11 +22,19 @@ export const Sidebar = styled.aside`
     height: 100vh;
     width: 38%;
 `
-export const SidebarContainer = styled.div``
+export const SidebarContainer = styled.div`
+    width: 500px;
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+`
 
 export const LogoContainer = styled.div``
 
-export const Logo = styled.img``
+export const Logo = styled.img`
+    width: 100%;
+`
 
 export const LoginContent = styled.main`
     width: 62%;
@@ -30,16 +44,53 @@ export const LoginContent = styled.main`
     justify-content: center;
     align-items: center;
 `
-export const LoginBox = styled.div``
+export const LoginBox = styled.div`
+    width: 460px;
+`
 
 export const FormBox = styled.form``
 
-export const FormItem = styled.div``
+export const FormItem = styled.div`
+    padding-top: 20px;
+    &:last-of-type {
+        margin-bottom: 20px;
+    }
+`
 
-export const FormLabel = styled.label``
+export const FormLabel = styled.label`
+    font-size: 14px;
+    font-weight: 700;
+    line-height: 125%;
+`
 
-export const FormInput = styled.input``
+export const FormInput = styled.input`
+    display: block;
+    width: calc(100% - 30px);
+    margin-top: 5px;
+    padding: 0 15px;
+    border: none;
+    height: 36px;
+    font-size: 14px;
+    line-height: 150%;
+    font-weight: 400;
+    border-radius: 4px;
+    background-color: #f4f4f4;
+`
 
-export const FormButton = styled(Link)``
+export const FormButton = styled(Link)`
+    display: block;
+    margin: auto;
+    text-align: center;
+    width: 140px;
+    height: 44px;
+    border: 1px solid transparent;
+    border-radius: 22px;
+    background-color: #1F0049;
+    color: #FFFFFF;
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 44px;
+    text-decoration: none;
+`
 
 export const ErrorMessage = styled.p``
